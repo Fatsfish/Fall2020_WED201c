bb88f8a74fb4d3f3433ae2a1f9061c25d4d0c381
Fatsfish <tuankhai512@gmail.com>
2021-07-17 09:17:06 +0700

    Initial commit

---
+++ Given Materials/WED201C_FA20_PE1_121212/PaperNo_2/All/Resource/mycode.js
@@ -0,0 +1,4 @@
+function show(id){
+		alert("The page is using cookie");
+		document.querySelector("#demo").visibility=visible;
+	}
---
+++ gallery.js
@@ -0,0 +1,10 @@
+function upDate(previewPic)
+{
+	document.querySelector('#image').style.backgroundImage = "url('" + previewPic.src +"')"; 
+	document.getElementById('image').innerHTML=previewPic.alt; 
+}
+
+function unDo()
+{   document.querySelector('#image').style.backgroundImage = "url('')";
+    document.querySelector('#image').innerHTML="Hover over an image below to display here."; 
+}
